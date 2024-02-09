b634c9fc8a4cd0c605d2d1ffc1ce87d40e4ea76a
Adiutant <softwarelines@gmail.com>
2024-02-10 00:08:07 +0300

    начало добавления интеграции ЛЛМ Гигачат для суммаризации и разбивки на токены

--- app/llm/llm_processing.py
+++ app/llm/llm_processing.py
@@ -10,11 +10,17 @@ def prompt_summarize(text):
               f" {text}")
     return prompt
 
+def prompt_summarize_job(text):
+    topics = "опыт работы, образование, ключевые навыки, дополнительная информация"
+    prompt = ("Разбей текст описания вакансии на темы и напиши в каждой теме предложения из текста вакансии соответствующие "
+              "этим темам."
+              f" Темы на которые надо разбить это необходимые {topics}"
+              f" {text}")
+    return prompt
+
 
 class LlmProcessor:
-    def __init__(self, file_data):
-        prompt = prompt_summarize(file_data)
-        print(prompt)
+    def __init__(self, file_data, prompt):
         creds = "NGRiOTkxNWUtMWI0MS00Yzk2LTkxMzQtZWYwMDJlNDI1ZTc4OmY0MmFhNDEzLTVkN2YtNGJlOC1iODg4LTA1NGU2ZmU3NTdjZg==" #os.getenv("GIGACHAT_API_KEY")#
         if creds is None:
             print("Set env GIGACHAT_API_KEY to your api key")
--- app/pipeline.py
+++ app/pipeline.py
@@ -8,12 +8,16 @@ def perform_pipeline(jobs_dir, cv):
     with open(cv) as f:
         lines = f.readlines()
         text = "".join(lines)
-        llm.llm_processing.LlmProcessor(text)
+        prompt = llm.llm_processing.prompt_summarize(text)
+        llm.llm_processing.LlmProcessor(text, prompt)
     only_files = [jobs_dir + f for f in os.listdir(jobs_dir) if isfile(join(jobs_dir, f))]
     for file in only_files:
         with open(file) as f:
             lines = f.readlines()
             text = "".join(lines)
             pre_processing.PreProcessor(text)
+            prompt = llm.llm_processing.prompt_summarize_job(text)
+            llm.llm_processing.LlmProcessor(text, prompt)
+
 
 
