207003e9b20e56bd4ad5885b9ea61ddbd9721c2d
Adiutant <softwarelines@gmail.com>
2024-02-09 23:00:59 +0300

    начало добавления интеграции ЛЛМ Гигачат для суммаризации и разбивки на токены

--- app/pipeline.py
+++ app/pipeline.py
@@ -5,11 +5,15 @@ import llm.llm_processing
 
 
 def perform_pipeline(jobs_dir, cv):
+    with open(cv) as f:
+        lines = f.readlines()
+        text = "".join(lines)
+        llm.llm_processing.LlmProcessor(text)
     only_files = [jobs_dir + f for f in os.listdir(jobs_dir) if isfile(join(jobs_dir, f))]
     for file in only_files:
         with open(file) as f:
             lines = f.readlines()
             text = "".join(lines)
             pre_processing.PreProcessor(text)
-            llm.llm_processing.LlmProcessor(text)
+
 
