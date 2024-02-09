534772a856a290c642197f13b49c99168f607550
Adiutant <softwarelines@gmail.com>
2024-02-10 00:15:40 +0300

    поменял порядок в пайплайне токенизация начинается после суммаризации

--- app/llm/llm_processing.py
+++ app/llm/llm_processing.py
@@ -10,21 +10,25 @@ def prompt_summarize(text):
               f" {text}")
     return prompt
 
+
 def prompt_summarize_job(text):
     topics = "опыт работы, образование, ключевые навыки, дополнительная информация"
-    prompt = ("Разбей текст описания вакансии на темы и напиши в каждой теме предложения из текста вакансии соответствующие "
-              "этим темам."
-              f" Темы на которые надо разбить это необходимые {topics}"
-              f" {text}")
+    prompt = (
+        "Разбей текст описания вакансии на темы и напиши в каждой теме предложения из текста вакансии соответствующие "
+        "этим темам."
+        f" Темы на которые надо разбить это необходимые {topics}"
+        f" {text}")
     return prompt
 
 
 class LlmProcessor:
+    result = None
+
     def __init__(self, file_data, prompt):
-        creds = "NGRiOTkxNWUtMWI0MS00Yzk2LTkxMzQtZWYwMDJlNDI1ZTc4OmY0MmFhNDEzLTVkN2YtNGJlOC1iODg4LTA1NGU2ZmU3NTdjZg==" #os.getenv("GIGACHAT_API_KEY")#
+        creds = os.getenv("GIGACHAT_API_KEY")
         if creds is None:
             print("Set env GIGACHAT_API_KEY to your api key")
             exit(1)
         with GigaChat(credentials=creds, verify_ssl_certs=False) as giga:
             response = giga.chat(prompt)
-            print(response.choices[0].message.content)
+            self.result = response.choices[0].message.content
--- app/pipeline.py
+++ app/pipeline.py
@@ -9,15 +9,18 @@ def perform_pipeline(jobs_dir, cv):
         lines = f.readlines()
         text = "".join(lines)
         prompt = llm.llm_processing.prompt_summarize(text)
-        llm.llm_processing.LlmProcessor(text, prompt)
+        print("summarizing candidate CV")
+        summary = llm.llm_processing.LlmProcessor(text, prompt).result
+        print(summary)
     only_files = [jobs_dir + f for f in os.listdir(jobs_dir) if isfile(join(jobs_dir, f))]
     for file in only_files:
         with open(file) as f:
             lines = f.readlines()
             text = "".join(lines)
-            pre_processing.PreProcessor(text)
             prompt = llm.llm_processing.prompt_summarize_job(text)
-            llm.llm_processing.LlmProcessor(text, prompt)
-
-
+            print("summarizing job description")
+            summary = llm.llm_processing.LlmProcessor(text, prompt).result
+            print(summary)
+            print("NLP preprocessing pipeline")
+            pre_processing.PreProcessor(summary)
 
