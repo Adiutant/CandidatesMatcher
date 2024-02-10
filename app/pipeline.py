2c8042d2789d5852c514ac39ff071ac356f2d73e
Роман <lagynkov.rom@gmail.com>
2024-02-10 22:30:12 +0300

    Preprocessing/topics tokenization (#4)
    * начало добавления интеграции ЛЛМ Гигачат для суммаризации и разбивки на топики
    
    * начало добавления интеграции ЛЛМ Гигачат для суммаризации и разбивки на токены
    
    * начало добавления интеграции ЛЛМ Гигачат для суммаризации и разбивки на токены
    
    * начало добавления интеграции ЛЛМ Гигачат для суммаризации и разбивки на токены
    
    * поменял порядок в пайплайне токенизация начинается после суммаризации
    
    * Добавил разбивку суммари на топики и токенизацию каждого топика
    
    ---------
    
    Co-authored-by: Adiutant <softwarelines@gmail.com>

--- app/pipeline.py
+++ app/pipeline.py
@@ -1,8 +1,7 @@
 import os
 from os.path import join, isfile
-import pre_processing
 import llm.llm_processing
-
+import topics_data
 
 def perform_pipeline(jobs_dir, cv):
     with open(cv) as f:
@@ -12,6 +11,18 @@ def perform_pipeline(jobs_dir, cv):
         print("summarizing candidate CV")
         summary = llm.llm_processing.LlmProcessor(text, prompt).result
         print(summary)
+        topics = topics_data.TopicsData(summary)
+        topics.perform_pre_processing()
+        print("tokenized topics")
+        print("Опыт работы")
+        print(topics.experience_processed)
+        print("Образование")
+        print(topics.education_processed)
+        print("Навыки")
+        print(topics.skills_processed)
+        print("Дополнительно")
+        print(topics.extra_processed)
+
     only_files = [jobs_dir + f for f in os.listdir(jobs_dir) if isfile(join(jobs_dir, f))]
     for file in only_files:
         with open(file) as f:
@@ -22,5 +33,15 @@ def perform_pipeline(jobs_dir, cv):
             summary = llm.llm_processing.LlmProcessor(text, prompt).result
             print(summary)
             print("NLP preprocessing pipeline")
-            pre_processing.PreProcessor(summary)
+            topics = topics_data.TopicsData(summary)
+            topics.perform_pre_processing()
+            print("tokenized topics")
+            print("Опыт работы")
+            print(topics.experience_processed)
+            print("Образование")
+            print(topics.education_processed)
+            print("Навыки")
+            print(topics.skills_processed)
+            print("Дополнительно")
+            print(topics.extra_processed)
 
--- app/pre_processing.py
+++ app/pre_processing.py
@@ -1,20 +1,51 @@
 import os
+import string
+from langdetect import detect
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 
 
+def detect_punctuation(word):
+    for ch in word:
+        if ch in string.punctuation:
+            return True
+    return False
+
+
+def detect_numbers(word):
+    for ch in word:
+        if ch.isdigit():
+            return True
+    return False
+
+
+def detect_notalpha(word):
+    for ch in word:
+        if not ch.isalpha():
+            return True
+    return False
+
+
+def detect_foreign_or_special(word):
+    if detect_punctuation(word) or detect_numbers(word) or detect_notalpha(word):
+        return True
+    return not detect(word) == 'ru'
+
+
 class PreProcessor:
     tokens = []
     stop_words = []
 
-    def __init__(self, file_data):
+    def __init__(self, text):
+        if text is None:
+            return
         nltk.download('punkt')
-        tokens = word_tokenize(file_data, "russian")
+        self.tokens = word_tokenize(text, "russian")
         nltk.download('stopwords')
         stop_words = set(stopwords.words("russian"))
-        tokens = [word for word in tokens if word not in stop_words]
+        self.tokens = [token for token in self.tokens if not detect_punctuation(token) or len(token) > 1]
+        self.tokens = [word for word in self.tokens if word not in stop_words]
         stemmer = SnowballStemmer("russian")
-        tokens = [stemmer.stem(word) for word in tokens]
-        print(tokens)
+        self.tokens = [stemmer.stem(word) if detect_foreign_or_special(word) else word for word in self.tokens]
